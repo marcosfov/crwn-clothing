c7d8f08db43c2008838a92dbc35416f69d0d52b6
Marcos Valeriano <marcosfov@outlook.com>
2023-03-23 20:03:55 -0300

    Adicionando route na home

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom"
 import { BackgroundImage, Body, DirectoryItemContainer } from "./directory-item.styles.jsx"
 
 export default function DirectoryItem({ category }) {
-  const { title, imageUrl } = category
+  const { title, imageUrl, route } = category
+  const navigate = useNavigate()
+
+  const onNavigateHandler = () => navigate(route)
 
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
